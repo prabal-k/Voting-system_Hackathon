79913c2c5e741e194ddaa27cddf1754e9da18afc
simonpok <poksimon9@gmail.com>
2023-12-06 13:03:42 +0545

    Few design changes and voting mainmenu

--- voting/src/Main.java
+++ voting/src/Main.java
@@ -1,16 +1,19 @@
 import javax.swing.*;
 
 public class Main {
-    static JFrame frame = new JFrame();
+   static Menu menu ;
+
+
     public static void main(String[] args)
     {
-        frame.setSize(600,600);
-        frame.setSize(800,700);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        //for textfield
-        frame.setVisible(true);
+        menu=new Menu();
+//        frame.setSize(600,600);
+//        frame.setSize(800,700);
+//        frame.setLocationRelativeTo(null);
+//        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//
+//        //for textfield
+//        frame.setVisible(true);
 
 
 
