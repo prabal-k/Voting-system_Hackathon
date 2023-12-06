16571cbd254b4f1d1ce8877078ba635976fb5ac2
prabal kuinkel <prabalkuinkel10@gmail.com>
2023-12-06 10:13:24 +0700

    Frame setup

--- voting/src/Main.java
+++ voting/src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    static JFrame frame = new JFrame();
+    public static void main(String[] args)
+    {
+        frame.setSize(600,600);
+        frame.setSize(800,700);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        //for textfield
+        frame.setVisible(true);
+
     }
 }
